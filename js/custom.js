a7e3c2aa719b0d52840a70520f82908c4cbc2b40
mathanram1 <meetmemathanram@gmail.com>
2023-08-21 12:49:52 +0530

    new

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,5 @@ console.log("hello world", 20, 20.7, true);
 const x = 120;
 console.log(x);
 console.warn("you are fucked");
-y = Math.floor(Math.random() * 1000000000);
+y = Math.ceil(Math.random() * 1000000000);
 console.log(y);
